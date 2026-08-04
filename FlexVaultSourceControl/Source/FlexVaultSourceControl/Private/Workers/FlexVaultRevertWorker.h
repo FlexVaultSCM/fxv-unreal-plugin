1451eb6f1c63d8e0491c558037e639cc3e595d70
Matthew Church <churchmf@gmail.com>
2026-08-04 08:26:28 -0600

    fix: grant WorkerRevertSidecarFallback test access to RevertedFiles
    Compile error: RevertedFiles is private on FFlexVaultRevertWorker, and only
    existing test classes were friended. Add the new sidecar-fallback test class.

--- FlexVaultSourceControl/Source/FlexVaultSourceControl/Private/Workers/FlexVaultRevertWorker.h
+++ FlexVaultSourceControl/Source/FlexVaultSourceControl/Private/Workers/FlexVaultRevertWorker.h
@@ -22,6 +22,7 @@ public:
 private:
 	friend class FFlexVaultWorkerRevertTest;
 	friend class FFlexVaultConflictRevertTransitionTest;
+	friend class FFlexVaultWorkerRevertSidecarFallbackTest;
 
 	mutable TArray<FString> RevertedFiles;
 };
